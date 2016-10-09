e9f87abb198ac9403d4da35f8e0fa887beaee086
Samuel Babalola <sammytech.net@gmail.com>
2016-10-09 18:12:43 -0400

    added image

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -43,6 +43,7 @@ export default class Layout extends React.Component {
       <div id="start-page">
         <div class="banner">
         	<Slider {...settings}>
+            <img src='img/ladies_room' />
           	<img src='http://placehold.it/1000x1000' />
             <img src='http://www.templateof.com/postpic/2015/05/success-website-banner_269676.png' />
             <img src='http://www.templateof.com/postpic/2015/05/backgrounds-design-web-banners_269681.jpg'/>
